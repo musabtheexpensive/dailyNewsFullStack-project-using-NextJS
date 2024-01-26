fb7f849167f00c15789fecec733105301ec213fb
Musab Al Mahi <musabalmahi53@gmail.com>
2024-01-26 23:52:54 +0600

    included button and button's related data showed in the category routes

--- src/app/categories/[categoriesid]/page.js
+++ src/app/categories/[categoriesid]/page.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const DynamicNewsPage = () => {
+const DynamicNewsPage = ({params,searchParams}) => {
     return (
         <div>
-            <h1>Dynamic News Page</h1>
+            <h1>Dynamic News Page {searchParams.category}</h1>
         </div>
     );
 };
--- src/components/ui/CategoryList/CategoryList.js
+++ src/components/ui/CategoryList/CategoryList.js
@@ -1,13 +1,24 @@
 import { getAllCategories } from "@/utils/getAllCategories";
+// import { Category } from "@mui/icons-material";
+import { Box, Button, Divider, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 import React from "react";
 
 const CategoryList = async () => {
   const { data: allCategories } = await getAllCategories();
   console.log(allCategories);
   return (
-    <div>
-      <h1>categories</h1>
-    </div>
+    <Box className="mt-5 bg-gray-100 px-5 py-2">
+      <Typography variant="h6">Categories</Typography>
+      <Divider />
+      <Stack rowGap={1} sx={{ mt: 2.5 }}>
+        {allCategories.map((category) => (
+          <Button variant="outlined" key={category.id}>
+            <Link href={`/categories/news?category=${category.title.toLowerCase()}`}>{category.title}</Link>
+          </Button>
+        ))}
+      </Stack>
+    </Box>
   );
 };
 
