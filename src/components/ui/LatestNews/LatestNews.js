78e936247f7c232bd411e67d0a8a7e2ef5bb5e9e
Musab Al Mahi <musabalmahi53@gmail.com>
2024-01-27 20:21:50 +0600

    The project is completely ready for deployment

--- src/components/ui/LatestNews/LatestNews.js
+++ src/components/ui/LatestNews/LatestNews.js
@@ -7,145 +7,82 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
-import topNews from "@/assets/download2.jpg";
-import topNews2 from "@/assets/download1.jpg";
+// import topNews from "@/assets/download2.jpg";
+// import topNews2 from "@/assets/download1.jpg";
 import Image from "next/image";
-const LatestNews = () => {
+import { getAllNews } from "@/utils/getAllNews";
+const LatestNews = async () => {
+  const { data } = await getAllNews();
+  console.log(data[0]);
   return (
     <Box className="my-5">
       <Card>
         <CardActionArea>
           <CardMedia>
-            <Image src={topNews} width={800} alt="top news" />
+            <Image
+              src={data[0].thumbnail_url}
+              width={800}
+              height={800}
+              alt="top news"
+            />
           </CardMedia>
           <CardContent>
             <p className="w-[100px] bg-red-500 px-2 text-white my-5 rounded">
-              Technology
+              {data[0].category}
             </p>
             <Typography gutterBottom variant="h5" component="div">
-              Bitcoin climbs as Elon Musk Says Tesla Likely to Accept it Again
+              {data[0].title}
             </Typography>
             <Typography className="my-3" gutterBottom>
-              By Musab Al Mahi -Mar 18 2024{" "}
+              By {data[0].author.name} - {data[0].author.published_date}
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              It IS A Long Established Fact That a Reader Will Be Distracted By
-              The Readable Content Of A Page when Looking At Its Layout........
+              {data[0].details.length > 200
+                ? data[0].details.slice(0, 200) + "..."
+                : data[0].details}
+              ...
             </Typography>
           </CardContent>
         </CardActionArea>
       </Card>
-      
+
       <Grid
         className="mt-5"
         container
         rowSpacing={1}
         columnSpacing={{ xs: 1, sm: 2, md: 3 }}
       >
-        <Grid item xs={6}>
-          <Card>
-            <CardActionArea>
-              <CardMedia>
-                <Image src={topNews2} width={800} alt="top news" />
-              </CardMedia>
-              <CardContent>
-                <p className="w-[100px] bg-red-500 px-2 text-white my-5 rounded">
-                  Technology
-                </p>
-                <Typography>
-                  Bitcoin climbs as Elon Musk Says Tesla Likely to Accept it
-                  Again
-                </Typography>
-                <Typography className="my-3" gutterBottom>
-                  By Musab Al Mahi -Mar 18 2024{" "}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  It IS A Long Established Fact That a Reader Will Be Distracted
-                  By The Readable Content Of A Page when Looking At Its
-                  Layout........
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardActionArea>
-              <CardMedia>
-                <Image src={topNews2} width={800} alt="top news" />
-              </CardMedia>
-              <CardContent>
-                <p className="w-[100px] bg-red-500 px-2 text-white my-5 rounded">
-                  Technology
-                </p>
-                <Typography>
-                  Bitcoin climbs as Elon Musk Says Tesla Likely to Accept it
-                  Again
-                </Typography>
-                <Typography className="my-3" gutterBottom>
-                  By Musab Al Mahi -Mar 18 2024{" "}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  It IS A Long Established Fact That a Reader Will Be Distracted
-                  By The Readable Content Of A Page when Looking At Its
-                  Layout........
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardActionArea>
-              <CardMedia>
-                <Image src={topNews2} width={800} alt="top news" />
-              </CardMedia>
-              <CardContent>
-                <p className="w-[100px] bg-red-500 px-2 text-white my-5 rounded">
-                  Technology
-                </p>
-                <Typography>
-                  Bitcoin climbs as Elon Musk Says Tesla Likely to Accept it
-                  Again
-                </Typography>
-                <Typography className="my-3" gutterBottom>
-                  By Musab Al Mahi -Mar 18 2024{" "}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  It IS A Long Established Fact That a Reader Will Be Distracted
-                  By The Readable Content Of A Page when Looking At Its
-                  Layout........
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card>
-            <CardActionArea>
-              <CardMedia>
-                <Image src={topNews2} width={800} alt="top news" />
-              </CardMedia>
-              <CardContent>
-                <p className="w-[100px] bg-red-500 px-2 text-white my-5 rounded">
-                  Technology
-                </p>
-                <Typography>
-                  Bitcoin climbs as Elon Musk Says Tesla Likely to Accept it
-                  Again
-                </Typography>
-                <Typography className="my-3" gutterBottom>
-                  By Musab Al Mahi -Mar 18 2024{" "}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  It IS A Long Established Fact That a Reader Will Be Distracted
-                  By The Readable Content Of A Page when Looking At Its
-                  Layout........
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </Grid>
+        {data.slice(0, 6).map((news) => (
+          <Grid item xs={6} key={news.id}>
+            <Card>
+              <CardActionArea>
+                <CardMedia>
+                  <Image
+                    src={news.thumbnail_url}
+                    height={500}
+                    width={800}
+                    alt="top news"
+                  />
+                </CardMedia>
+                <CardContent>
+                  <p className="w-[100px] bg-red-500 px-2 text-white my-5 rounded">
+                    {news.category}
+                  </p>
+                  <Typography>{news.title}</Typography>
+                  <Typography className="my-3" gutterBottom>
+                    By {data[0].author.name} - {data[0].author.published_date}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {data[0].details.length > 200
+                      ? data[0].details.slice(0, 200) + "..."
+                      : data[0].details}
+                    ...
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
