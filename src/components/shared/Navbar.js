9f3deb38a64a97be3445eb95610f22a1c6af9dcd
Musab Al Mahi <musabalmahi53@gmail.com>
2024-01-28 00:10:36 +0600

    again and again facing a issue but never solved it yet

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -43,14 +43,14 @@ function Navbar() {
   return (
    <>
    <Header/>
-    <AppBar position="static" className="bg-[yellow]">
+    <AppBar position="static" className="bg-gray-400">
       <Container maxWidth="xl">
-        <Toolbar disableGutters>
+        <Toolbar >
           <Image src={logo} width={80} height={80} alt="logo" />
           <Box className="w-full text-center">
             {navItems.map((item) => (
               <Link key={item} href={item.pathname}>
-                <Button className="text-black">{item.route}</Button>
+                <Button className="text-white-200">{item.route}</Button>
               </Link>
             ))}
           </Box>
